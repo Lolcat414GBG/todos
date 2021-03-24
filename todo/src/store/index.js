47dc9ce3b16ab9452c9a60a887781810fa83c8ce
jensengbg-linda-jansson <linda.t.jansson@gmail.com>
2021-03-24 16:07:30 +0100

    updates

--- todo/src/store/index.js
+++ todo/src/store/index.js
@@ -5,7 +5,8 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    todos: []
+    todos: [],
+    url: 'https://awesome-todo-api.herokuapp.com/tasks'
   },
   mutations: {
     addTodos(state, todos) {
@@ -14,29 +15,24 @@ export default new Vuex.Store({
   },
   actions: {
     async getTodos(ctx) {
-      const url = 'https://awesome-todo-api.herokuapp.com/tasks';
-      const response = await fetch(url, { method: 'GET' });
+      const response = await fetch(this.state.url, { method: 'GET' });
       const data = await response.json();
       ctx.commit('addTodos', data.todos);
     },
     async addTodo(ctx, todo) {
       const obj = { task: todo }
-      const response = await fetch('https://awesome-todo-api.herokuapp.com/tasks', 
+      const response = await fetch(this.state.url, 
       { method: 'POST',
         body: JSON.stringify(obj),
         headers: {'Content-Type': 'application/json'}
       });
       const data = await response.json();
-      console.log('addTodo: ', data);
-      ctx.dispatch('getTodos');
+      ctx.commit('addTodos', data.todo);
     },
     async removeTodo(ctx, id) {
-      console.log('removeTodo: ', ctx, id);
-      const url = 'https://awesome-todo-api.herokuapp.com/tasks/' + id;
-      const response = await fetch(url, { method: 'DELETE' });
+      const response = await fetch(this.state.url + '/' + id, { method: 'DELETE' });
       const data = await response.json();
-      console.log('ny data i removeTodo', data);
-      ctx.dispatch('getTodos');
+      ctx.dispatch('getTodos', data);
     }
   },
   modules: {
