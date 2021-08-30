730248ce9ed7b82fd19b6998400eecc439289a58
jensengbg-linda-jansson <linda.t.jansson@gmail.com>
2021-08-30 13:43:25 +0200

    Minor fixes

--- todo/src/App.vue
+++ todo/src/App.vue
@@ -7,22 +7,27 @@
 </template>
 
 <script>
-import AddTodo from './components/AddTodo.vue'
-import Todos from './components/Todos.vue'
+import AddTodo from './components/AddTodo.vue';
+import Todos from './components/Todos.vue';
 
 export default {
   name: 'App',
   components: {
     AddTodo,
-    Todos
+    Todos,
   },
   created() {
     this.$store.dispatch('getTodos');
-  }
-}
+  },
+};
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -30,5 +35,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1 {
+  padding: 1em 0 0;
 }
 </style>
